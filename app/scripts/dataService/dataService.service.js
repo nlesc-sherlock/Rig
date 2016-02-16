4e5486bb39bed8031df50b2bac13227a592ed347
Willem van Hage <w.vanhage@esciencecenter.nl>
2016-02-16 16:27:15 +0100

    Added main.css

--- app/scripts/dataService/dataService.service.js
+++ app/scripts/dataService/dataService.service.js
@@ -2,7 +2,7 @@
   'use strict';
 
   function DataService($q, $http, $timeout, Messagebus) { //, AuthenticationService) {
-    this.backendURL = 'http://localhost:5000/query/';
+    this.backendURL = 'http://localhost:5000/';
 
     this.exampleQuery = 'example';
 
@@ -14,15 +14,20 @@
     };
 
     this.doQuery = function() {
-      Messagebus.publish('dataUpdate', { 
+      $http.get(encodeURI(this.backendURL)).then(function(result){
+        Messagebus.publish('dataUpdate',result.data);
+      });
+      //Messagebus.publish('dataUpdate', ... );
+/*    { 
         'columns': [ 'one', 'two', 'three' ],
         'data': [
           { 'one': 1, 'two': 2, 'three': 3 },
           { 'one': 4, 'two': 5, 'three': 6 },
           { 'one': 7, 'two': 8, 'three': 9 }
-        ]});
+        ]}
+  */              
 //      return $http.get(encodeURI(this.backendURL + this.exampleQuery));
-    };
+    }.bind(this);
 
   }
 
