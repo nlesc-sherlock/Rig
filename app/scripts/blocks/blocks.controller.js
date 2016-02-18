25a5b057f66a435364925f4d0fe532e53fed178c
Willem van Hage <w.vanhage@esciencecenter.nl>
2016-02-18 13:47:41 +0100

    First steps to dynamic suggestions

--- app/scripts/blocks/blocks.controller.js
+++ app/scripts/blocks/blocks.controller.js
@@ -2,22 +2,7 @@
   'use strict';
 
   function BlocksController(Messagebus, $scope) {
-    this.suggestedBlocks = [];
-    this.topOfStack = {};
-
-    Messagebus.subscribe('mainScreenInteraction',function(event,colHeader) {
-      console.log('blocks sais ' + colHeader);
-      this.suggestedBlocks = [
-        {'operation':'split','parameter':colHeader},
-        {'operation':'sample','parameter':100}
-      ];
-    }.bind(this));
-    
-    Messagebus.subscribe('topOfStack',function(event,block) {
-      console.log('new top of stack is');
-      console.log(block);
-      this.topOfStack = block;
-    }.bind(this));
+    this.suggestedBlocks = []; /* output of the DataService suggestions */
 
     this.planBlock = function(block) {
       console.log('planning the block');
@@ -25,6 +10,10 @@
       Messagebus.publish('planBlock',block);
     };
 
+    Messagebus.subscribe('newSuggestions',function(event,suggestions) {
+      this.suggestedBlocks = suggestions;
+    }.bind(this));
+    
   }
   angular.module('rigApp.blocks')
     .controller('BlocksController', BlocksController);
--- app/scripts/dataService/dataService.service.js
+++ app/scripts/dataService/dataService.service.js
@@ -5,18 +5,39 @@
     this.backendURL = 'http://localhost:5000';
 
     this.exampleQuery = '/records/original/0/100';
+    this.suggestionsPath = '/suggestions/';
+
+    this.currentTopOfStack = {};
+    this.lastMainScreenInteraction = {};
 
     var deferred = $q.defer();
     this.ready = deferred.promise;
 
     this.init = function() {
-      $timeout(this.doQuery,10);
+      $timeout(this.getData,10);
     };
 
-    this.doQuery = function() {
+    this.getData = function() {
       Messagebus.publish('dataUpdate',$http.get(encodeURI(this.backendURL + this.exampleQuery)));
     }.bind(this);
 
+    this.getSuggestions = function(topOfStack,mainScreenInteraction) {
+      Messagebus.publish('blockSuggestions',$http.post(encodeURI(this.backendURL + this.suggestionsPath)),
+        {
+          'topOfStack': topOfStack,
+          'mainScreenInteraction': mainScreenInteraction
+        });
+    }.bind(this);
+
+    Messagebus.subscribe('mainScreenInteraction',function(event,interactionSpec) {
+      this.lastMainScreenInteraction = interactionSpec;
+      this.getSuggestions(this.currentTopOfStack,interactionSpec);
+    }.bind(this));
+    
+    Messagebus.subscribe('topOfStack',function(event,block) {
+      this.currentTopOfStack = block;
+      this.getSuggestions(this.currentTopOfStack,this.lastMainScreenInteraction);
+    }.bind(this));
 
   }
 
