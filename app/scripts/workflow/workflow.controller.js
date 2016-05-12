a2306e8d075dbd52a030946d070a3e91269d9bb7
Willem van Hage <w.vanhage@esciencecenter.nl>
2016-05-12 10:28:37 +0200

    Filled in arguments captured in blocks

--- app/scripts/workflow/workflow.controller.js
+++ app/scripts/workflow/workflow.controller.js
@@ -13,6 +13,7 @@
 
     this.execute = function() {
       console.log('execute');
+      console.log(this.workflowBlocks);
       window.alert('EXECUTING WORKFLOW\nPLEASE WAIT\n\n' + JSON.stringify(this.workflowBlocks,null,'    '));
     }
   }
